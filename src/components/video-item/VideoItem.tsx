92ba121a6942b6aedfd53b7a17f34bb7bcc4046d
Amos <asidelnik@gmail.com>
2023-10-26 12:40:15 +0300

    VideoItem - comment all code

--- src/components/video-item/VideoItem.tsx
+++ src/components/video-item/VideoItem.tsx
@@ -1,50 +1,50 @@
-import { useEffect, useRef } from "react";
-import { Video } from "../../types/video"
-import YTPlayer from "yt-player";
+// import { Video } from "../../types/video"
+// import { useEffect, useRef } from "react";
+// import YTPlayer from "yt-player";
 
-type Props = {
-  video: Video;
-  isSyncPlay: boolean;
-  timelineTime: number;
-  timelineStartTime: Date;
-};
+// type Props = {
+//   video: Video;
+//   isSyncPlay: boolean;
+//   timelineTime: number;
+//   timelineStartTime: Date;
+// };
 
-export default function VideoItem({
+export default function VideoItem(/*{
   video,
   isSyncPlay,
   timelineTime,
   timelineStartTime
-}: Props) {
-  const { orientation, url } = video;
-  const iframeRef = useRef<HTMLIFrameElement>(null);
+}: Props*/) {
+  // const { orientation, url } = video;
+  // const iframeRef = useRef<HTMLIFrameElement>(null);
 
-  useEffect(() => {
-    if (iframeRef.current) {
-      const player = new YTPlayer(iframeRef.current);
-    }
+  // useEffect(() => {
+  //   if (iframeRef.current) {
+  //     const player = new YTPlayer(iframeRef.current);
+  //   }
 
-    if (isSyncPlay) {
-      const isVideoStartTimeEqualToTimeLineTime = timelineTime * 1000 == video.startTime.getTime() - timelineStartTime.getTime()
-      if (isVideoStartTimeEqualToTimeLineTime) {
-        player.play();
-      }
-    } else {
-      if (player.getState() == "playing") {
-        player.pause();
-      }
-    }
-  }, [isSyncPlay, timelineTime]); // , timelineStartTime,
+  //   if (isSyncPlay) {
+  //     const isVideoStartTimeEqualToTimeLineTime = timelineTime * 1000 == video.startTime.getTime() - timelineStartTime.getTime()
+  //     if (isVideoStartTimeEqualToTimeLineTime) {
+  //       player.play();
+  //     }
+  //   } else {
+  //     if (player.getState() == "playing") {
+  //       player.pause();
+  //     }
+  //   }
+  // }, [isSyncPlay, timelineTime]); // , timelineStartTime,
 
   return (
     <>
-      <iframe
+      {/* <iframe
         ref={iframeRef}
         width={orientation == "Landscape" ? "560" : "315"}
         height={orientation == "Landscape" ? "315" : "560"}
         src={url}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      ></iframe>
+      ></iframe> */}
     </>
   )
 }
