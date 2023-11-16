20bd11629e07ffe3d659c86748ecd189db3aa9a0
Amos <asidelnik@gmail.com>
2023-11-16 14:00:46 +0200

    Events table page - added get events request & showed in ul successfully

--- src/pages/EventsTablePage.tsx
+++ src/pages/EventsTablePage.tsx
@@ -1,25 +1,49 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { EventType } from "../types/event";
+
 
 export default function EventsTablePage() {
-  const events: string[] = [
-    "jon-snow",
-    "johnny-bravo",
-    "john-wick",
-  ];
+  const [events, setEvents] = useState<EventType[] | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    const axiosFetchData = async () => {
+      try {
+        const url = `http://localhost:3000/events`;
+        const response = await axios.get(url);
+        const data = response?.data;
+        if (data) {
+          data.map((event: EventType) => {
+            event.startTime = new Date(event.startTime);
+            event.endTime = new Date(event.endTime);
+          });
+          setEvents(data);
+        }
+        setIsLoading(false);
+      } catch (error: any) {
+        setErrorMessage(error.message)
+        setIsError(true);
+        setIsLoading(false);
+      }
+    };
+    axiosFetchData();
+  }, []);
 
   return (
     <>
       <div className="temp-event-links">
         <ul>
-          {
-            events.map((event: string, index: number) => (
-              <li key={index}>
-                <Link to={`/events/${event}`}>{event}</Link>
-              </li>
-            ))
-          }
+          {events && events.map((event: EventType, index: number) => (
+            <li key={index}>
+              <Link to={`/events/${event.id}`}>{event.title}</Link>
+            </li>
+          ))}
         </ul>
       </div>
     </>
-  )
+  );
 }
