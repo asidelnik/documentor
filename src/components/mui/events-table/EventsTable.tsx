ff9e6693b1db249c53d164a71bf184e800b13a47
Amos <asidelnik@gmail.com>
2023-11-16 17:12:59 +0200

    >

--- src/components/mui/events-table/EventsTable.tsx
+++ src/components/mui/events-table/EventsTable.tsx
@@ -1,4 +1,4 @@
-import c from './EventsTable.module.scss';
+// import c from './EventsTable.module.scss';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
