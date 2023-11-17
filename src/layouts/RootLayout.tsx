d6b6d7a00f2a2ca005f16137c57bda77b8f11692
Amos <asidelnik@gmail.com>
2023-11-17 12:43:34 +0200

    useFetchDataTests - commented

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,9 +1,9 @@
 import { Outlet } from "react-router-dom";
 import RootHeader from "../headers/RootHeader";
-import useFetchDataTests from "../server/useFetchDataTests";
+// import useFetchDataTests from "../server/useFetchDataTests";
 
 export default function RootLayout() {
-  useFetchDataTests();
+  // useFetchDataTests();
   // console.log({ data });
 
   return (
