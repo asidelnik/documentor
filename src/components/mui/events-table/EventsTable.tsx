a8c6e650e4f5b8af5909fb12d101a353c7ed6fb0
Amos <asidelnik@gmail.com>
2023-11-17 12:43:06 +0200

    EventsTable - pagination added - not completed

--- src/components/mui/events-table/EventsTable.tsx
+++ src/components/mui/events-table/EventsTable.tsx
@@ -6,47 +6,73 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import TablePagination from '@mui/material/TablePagination';
+
 import { EventsTableProps } from '../../../props/eventsTableProps';
 import { EventType } from '../../../types/event';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 
 export default function EventsTable({ rows }: EventsTableProps) {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Title</TableCell>
-            <TableCell>Start time</TableCell>
-            <TableCell>End time</TableCell>
-            <TableCell>Description</TableCell>
-            <TableCell>Duration</TableCell>
-            <TableCell>Location</TableCell>
-            <TableCell>Status</TableCell>
-            <TableCell>Videos to review</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row: EventType) => (
-            <TableRow
-              key={row.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                <Link to={`/events/${row.id}`}>{row.title}</Link>
-              </TableCell>
-              <TableCell>{row.startTimeFormatted}</TableCell>
-              <TableCell>{row.endTimeFormatted}</TableCell>
-              <TableCell>{row.description}</TableCell>
-              <TableCell>{row.durationFormatted}</TableCell>
-              <TableCell>{row.locationName}</TableCell>
-              <TableCell>{row.statusFormatted}</TableCell>
-              <TableCell>{row.videosUnprocessedCount}</TableCell>
+    <Paper sx={{ width: '100%', mb: 2 }}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell>Start time</TableCell>
+              <TableCell>End time</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell>Duration</TableCell>
+              <TableCell>Location</TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell>Videos to review</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row: EventType) => (
+              <TableRow
+                key={row.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  <Link to={`/events/${row.id}`}>{row.title}</Link>
+                </TableCell>
+                <TableCell>{row.startTimeFormatted}</TableCell>
+                <TableCell>{row.endTimeFormatted}</TableCell>
+                <TableCell>{row.description}</TableCell>
+                <TableCell>{row.durationFormatted}</TableCell>
+                <TableCell>{row.locationName}</TableCell>
+                <TableCell>{row.statusFormatted}</TableCell>
+                <TableCell>{row.videosUnprocessedCount}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper >
   );
 }
--- src/pages/EventsTablePage.tsx
+++ src/pages/EventsTablePage.tsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import { EventType } from "../types/event";
 import EventsTable from "../components/mui/events-table/EventsTable";
 import { secondsToTimeString } from "../utils/functions";
 import { EventStatusEnum } from "../enums/event-status-enum";
+import { serverRoutes } from "../server/server-routes";
 
 
 export default function EventsTablePage() {
@@ -11,15 +11,28 @@ export default function EventsTablePage() {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const baseUrl = 'http://localhost:3000';
 
   useEffect(() => {
     const axiosFetchData = async () => {
       try {
-        const url = `http://localhost:3000/events`;
-        const response = await axios.get(url);
-        const data = response?.data;
-        if (data) {
-          data.map((event: EventType) => {
+        const getEventsRequestString = serverRoutes.getFilteredEvents({
+          fromDate: '2023-10-01',
+          toDate: '2023-10-27',
+          lat: 32.0853,
+          lon: 34.7818,
+          radius: 10,
+          status: 1,
+          page: 1,
+          limit: 3,
+          tags: ['tag1', 'tag2'],
+          tagsJoined: ''
+        });
+        const filteredEventsRes = await fetch(baseUrl + getEventsRequestString);
+        const filteredEvents = await filteredEventsRes.json();
+        // const data = response?.data;
+        if (filteredEvents) {
+          filteredEvents.map((event: EventType) => {
             event.startTime = new Date(event.startTime);
             event.endTime = new Date(event.endTime);
             event.startTimeFormatted = event.startTime.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
@@ -27,7 +40,7 @@ export default function EventsTablePage() {
             event.durationFormatted = secondsToTimeString(event.duration);
             event.statusFormatted = EventStatusEnum[event.status];
           });
-          setEvents(data);
+          setEvents(filteredEvents);
         }
         setIsLoading(false);
       } catch (error: any) {
