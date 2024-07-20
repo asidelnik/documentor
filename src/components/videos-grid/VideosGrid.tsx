f0f3f793e7fb1c31a0df1688dee335e77cf0a6bf
Amos <asidelnik@gmail.com>
2024-07-20 17:18:56 +0300

    VideosGrid - styling updates

--- src/components/videos-grid/VideosGrid.tsx
+++ src/components/videos-grid/VideosGrid.tsx
@@ -8,30 +8,34 @@ export default function VideosGrid({ videos, videosCount }: IVideosGridProps) {
   return (
     <>
       <div className={c.videoCount}>{videosCount} videos</div>
-      <div className={c.videoGridContainer}>
-        <div className={c.videoGrid}>
-          {/* TODO - Virtual scrolling */}
-          {videos.map((video: Video) => (
-            <div key={video.id} className={c.videoItem}>
-              <ReactPlayer
-                key={video.id}
-                url={video.url}
-                className={c.videoItem}
-                controls={true}
-                config={{
-                  youtube: {
-                    embedOptions: {
-                      width: "315",
-                      height: "177"
+      {videos && videos.length > 0 && (
+        <div className={c.container}>
+          <div className={c.videosGrid}>
+            {/* TODO - Virtual scrolling */}
+            {videos.map((video: Video) => (
+              <div key={video.id} className={c.video} >
+                <ReactPlayer
+                  key={video.id}
+                  url={video.url}
+                  className={c.videoPlayer}
+                  controls={true}
+                  width='100%'
+                  height='177px'
+                  config={{
+                    youtube: {
+                      embedOptions: {
+                        width: "315",
+                        height: "177" //video.orientation === OrientationEnum.Landscape ? "177" : "560"
+                      }
                     }
-                  }
-                }}
-              />
-              <VideoInfo video={video} />
-            </div>
-          ))}
+                  }}
+                />
+                <VideoInfo video={video} />
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </>
   )
 }
