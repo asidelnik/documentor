2d8b9701f9bb5d2a49ea1358b4f273db182caf80
Amos <asidelnik@gmail.com>
2024-07-21 15:48:15 +0300

    AllVideosPage - fetch videos request - AbortController

--- src/pages/AllVideosPage.tsx
+++ src/pages/AllVideosPage.tsx
@@ -2,9 +2,10 @@
 import { useEffect, useState } from "react";
 import { serverRoutes } from "../server/server-routes";
 import { Video } from "../types/video";
-import { GetVideosQueryParams } from "../types/getVideosQueryParams";
+import { IGetVideosFilters } from "../types/IGetVideosFilters";
 import VideosFilters from '../components/videos-filters/VideosFilters';
 import VideosGrid from '../components/videos-grid/VideosGrid';
+import { VideoStatusEnum } from "../constants/video-status";
 
 export default function AllVideosPage() {
   const baseUrl = import.meta.env.VITE_BASE_URL;
@@ -19,21 +20,27 @@ export default function AllVideosPage() {
     lat: 32.0853,
     lng: 34.7818,
     radius: 10,
-    status: undefined,
+    statuses: [VideoStatusEnum.Unprocessed],
     page: 1,
     limit: 100,
   }
+  // Abort fetchData request call if another call has been made before previous completion
+  let fetchController = new AbortController();
 
   useEffect(() => {
     fetchData(defaultFilters);
+    return () => fetchController.abort()
   }, [])
 
-  const fetchData = async (params: GetVideosQueryParams) => {
+  const fetchData = async (params: IGetVideosFilters) => {
+    fetchController.abort('Newer fetch called');
+    fetchController = new AbortController();
+    const { signal } = fetchController;
     try {
       const filteredParams = Object.fromEntries(Object.entries(params).filter(([_, value]) => value !== undefined));
       const getFilteredVideosRequestString = serverRoutes.getFilteredVideos(filteredParams);
 
-      const response = await fetch(baseUrl + getFilteredVideosRequestString);
+      const response = await fetch(baseUrl + getFilteredVideosRequestString, { signal });
       if (!response.ok) {
         throw new Error(response.statusText);
       }
@@ -50,6 +57,7 @@ export default function AllVideosPage() {
         setVideos(updatedVideos);
         setVideosCount(filteredVideos.videosCount);
       }
+
       setIsLoading(false);
     } catch (error) {
       console.log(error);
@@ -62,7 +70,9 @@ export default function AllVideosPage() {
   return (
     <>
       <VideosFilters defaultFilters={defaultFilters} fetchData={fetchData} />
-      <VideosGrid videos={videos} videosCount={videosCount} />
+      {videos && videos.length > 0 &&
+        <VideosGrid videos={videos} videosCount={videosCount} />
+      }
     </>
   )
 }
