ad46ec92ba824ec90cf50010fac4dfe0b2dbca53
Amos <asidelnik@gmail.com>
2024-08-01 13:28:49 +0300

    Bug: filters initial state - updated

--- src/initial-state/filtersInitialState.ts
+++ src/initial-state/filtersInitialState.ts
@@ -2,10 +2,8 @@ import { VideoStatusEnum } from '../constants/video-status';
 import { IVideosFilters } from '../types/IVideosFilters';
 
 export const filtersInitialState: IVideosFilters = {
-  fromDate: new Date('2023-10-01T00:00'), // For debugging
-  // fromDate: new Date(new Date().setMonth(new Date().getMonth() - 1)), // 1 month ago
-  toDate: new Date('2023-10-27T00:00'), // For debugging
-  // toDate: new Date(),
+  fromDate: new Date(new Date().setMonth(new Date().getMonth() - 1)), // 1 month ago
+  toDate: new Date(), // Today
   lat: 32.0853,
   lng: 34.7818,
   radius: 10,
