4b2ed79fcc1908233d56c3d4bc41861da8db2e9d
Amos <asidelnik@gmail.com>
2024-08-01 13:27:09 +0300

    comment

--- src/initial-state/filtersInitialState.ts
+++ src/initial-state/filtersInitialState.ts
@@ -2,8 +2,10 @@ import { VideoStatusEnum } from '../constants/video-status';
 import { IVideosFilters } from '../types/IVideosFilters';
 
 export const filtersInitialState: IVideosFilters = {
-  fromDate: new Date('2023-10-01T00:00'), // fromDate: new Date(new Date().setMonth(new Date().getMonth() - 1)), // 1 month ago
-  toDate: new Date('2023-10-27T00:00'), // toDate: new Date(),
+  fromDate: new Date('2023-10-01T00:00'), // For debugging
+  // fromDate: new Date(new Date().setMonth(new Date().getMonth() - 1)), // 1 month ago
+  toDate: new Date('2023-10-27T00:00'), // For debugging
+  // toDate: new Date(),
   lat: 32.0853,
   lng: 34.7818,
   radius: 10,
