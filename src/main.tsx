92c768a8e041da0e0583ab6687e7ea5f1389629e
Amos <asidelnik@gmail.com>
2024-08-02 15:44:20 +0300

    React Query - setup

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,15 @@ import { router } from "./router.tsx";
 import './index.css'
 import { ThemeProvider } from '@mui/material';
 import { muiTheme } from './components/mui/theme.tsx';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './query-client/queryClient.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={muiTheme}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
