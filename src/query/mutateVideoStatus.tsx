8178539511ab5339355f047fe2376ad80ee273cd
Amos <asidelnik@gmail.com>
2024-08-08 13:36:50 +0300

    chore: mutateVideoStatus - fetch - if !response.ok, return Error. For react query - mutation onError() call.

--- src/query/mutateVideoStatus.tsx
+++ src/query/mutateVideoStatus.tsx
@@ -1,9 +1,13 @@
 import { serverRoutes } from "../server/server-routes";
 
-export const mutateVideoStatus = ({ videoId, status }: IVideoStatusMutationProps) => {
+export const mutateVideoStatus = async ({ videoId, status }: IVideoStatusMutationProps) => {
   const baseUrl = import.meta.env.VITE_BASE_URL;
   const requestPath = serverRoutes.videos.videoSetStatus(videoId, status);
-  return fetch(baseUrl + requestPath, { method: 'PUT' });
+  const response = await fetch(baseUrl + requestPath, { method: 'PUT' });
+  if (!response.ok) {
+    throw new Error('Network error');
+  }
+  return response.json();
 };
 
 export interface IVideoStatusMutationProps {
