6baad94ae0f45c14ac28e30751622ce7382876e7
Amos <asidelnik@gmail.com>
2024-08-08 21:33:55 +0300

    chore: Video Event picker - Autocomplete - use simpler code & fixed option value bug.

--- src/shared/components/checkbox-tags/CheckboxTags.tsx
+++ src/shared/components/checkbox-tags/CheckboxTags.tsx
@@ -1,42 +1,26 @@
-import Checkbox from '@mui/material/Checkbox';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
-import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
-import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import { ICheckBoxesTagsProps } from '../../../props/ICheckBoxesTagsProps';
-
-const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
-const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
 export default function CheckboxesTags({ options, checkedId, update, isDisabled }: ICheckBoxesTagsProps) {
   const checkedOption = checkedId === null ? undefined : options.find(o => o.id === checkedId);
 
   return (
     <Autocomplete
+      disablePortal
       id="checkboxes-tags"
-      value={checkedOption}
+      value={checkedOption ?? null}
       options={options}
-      disableCloseOnSelect
-      limitTags={1}
+      style={{ width: "100%" }}
       disabled={isDisabled}
       getOptionLabel={(option) => option.title}
-      renderOption={(_, option) => {
-        return (
-          <li key={option.id}>
-            <Checkbox
-              icon={icon}
-              checkedIcon={checkedIcon}
-              style={{ marginRight: 8 }}
-              checked={option.id === checkedId}
-              onChange={() => update(option.id === checkedId ? null : option.id)}
-            />
-            {option.title}
-          </li>
-        );
-      }}
-      style={{ width: "100%" }}
+      onChange={(_event, value) => update(value?.id ?? null)}
+      // isOptionEqualToValue={(option, value) => option.id === value?.id}
       renderInput={(params) => (
-        <TextField {...params} placeholder="Event" value={checkedOption?.title ?? ''} style={{ height: 60 }} />
+        <TextField {...params}
+          placeholder="Event"
+          value={checkedOption?.title ?? ''}
+          style={{ height: 60 }} />
       )}
     />
   );
