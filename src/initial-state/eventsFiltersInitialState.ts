99aa681106469825951687cfc5de4b0f5cca58bc
Amos <asidelnik@gmail.com>
2024-11-19 21:28:22 +0200

    92 - chore: Client - events table - initial filtering - updated

--- src/initial-state/eventsFiltersInitialState.ts
+++ src/initial-state/eventsFiltersInitialState.ts
@@ -3,7 +3,7 @@ import { IEventsFilters } from '../types/IEventsFilters';
 
 export const eventsFiltersInitialState: IEventsFilters = {
   fromDate: new Date('2023-10-01T00:00'), // For debugging
-  toDate: new Date('2023-10-27T00:00'), // For debugging
+  toDate: new Date('2025-01-01T00:00'), // For debugging
   priority: [EventPriority.High, EventPriority.Medium, EventPriority.Low],
   freeText: '',
   statuses: [EventStatus.Active, EventStatus.Inactive],
