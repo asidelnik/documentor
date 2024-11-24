4ada39c527931aab7787cdb701efaeac3444ae66
Amos <asidelnik@gmail.com>
2024-11-24 19:55:00 +0200

    100 - fix: Adjust base URL in vite.config.ts for production and development modes

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/documentor/',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/documentor/' : '/',
+    plugins: [react()],
+  };
 });
