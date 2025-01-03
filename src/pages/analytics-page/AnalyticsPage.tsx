387ccbc4a308827a386e1bdbe62fec87e23097e7
Amos <asidelnik@gmail.com>
2025-01-03 17:42:04 +0200

    114 - feat: Analytics - Add pie chart and update bar chart with new data structure and options

--- src/pages/analytics-page/AnalyticsPage.tsx
+++ src/pages/analytics-page/AnalyticsPage.tsx
@@ -1,15 +1,18 @@
 import c from './AnalyticsPage.module.scss';
-import { Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
-import { data, options } from '../../initial-state/analyticsInitialState';
+import { Bar, Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement } from 'chart.js';
+import { barData, barOptions, pieData, pieOptions } from '../../initial-state/analyticsInitialState';
 import { useState } from 'react';
 import { IconButton } from '@mui/material';
 import FilterListRoundedIcon from '@mui/icons-material/FilterListRounded';
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement);
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
 
 export default function AnalyticsPage() {
   const [toggleAside, setToggleAside] = useState<boolean>(true);
+  const [year, setYear] = useState<number>(2023);
+
   return (
     <>
       <div className={c.container}>
@@ -20,12 +23,24 @@ export default function AnalyticsPage() {
             </IconButton>
           </div>
 
-          <div className={toggleAside ? 'visible' : 'hidden'}>
-          </div>
+          <div className={toggleAside ? 'visible' : 'hidden'}></div>
         </aside>
         <main>
-          <Bar data={data} options={options} />
-
+          <div>
+            <label htmlFor="year">Filter by Year: </label>
+            <input
+              type="number"
+              id="year"
+              value={year}
+              onChange={(e) => setYear(Number(e.target.value))}
+              min="2000"
+              max="2100"
+            />
+          </div>
+          <div style={{ width: '700px' }}>
+            <Bar data={barData} options={barOptions} />
+          </div>
+          <Pie data={pieData} options={pieOptions} />
         </main>
       </div>
     </>
