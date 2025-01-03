d2b2adc22983f222eff6dcd277e5120875fcc1f0
Amos <asidelnik@gmail.com>
2025-01-03 17:12:49 +0200

    114 - feat: Analytics - Add layout and styling for the analytics page with toggleable sidebar

--- src/pages/analytics-page/AnalyticsPage.tsx
+++ src/pages/analytics-page/AnalyticsPage.tsx
@@ -1,14 +1,33 @@
+import c from './AnalyticsPage.module.scss';
 import { Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, ChartOptions, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { data, options } from '../../initial-state/analyticsInitialState';
+import { useState } from 'react';
+import { IconButton } from '@mui/material';
+import FilterListRoundedIcon from '@mui/icons-material/FilterListRounded';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 export default function AnalyticsPage() {
+  const [toggleAside, setToggleAside] = useState<boolean>(true);
   return (
     <>
-      <h1>Analytics</h1>
-      <p>Analytics page content</p>
-      <Bar data={data} options={options} />
+      <div className={c.container}>
+        <aside className={toggleAside ? c.asideOpen : c.asideClose}>
+          <div className={c.asideToggle}>
+            <IconButton onClick={() => setToggleAside(!toggleAside)} aria-label="toggle aside">
+              <FilterListRoundedIcon />
+            </IconButton>
+          </div>
+
+          <div className={toggleAside ? 'visible' : 'hidden'}>
+          </div>
+        </aside>
+        <main>
+          <Bar data={data} options={options} />
+
+        </main>
+      </div>
     </>
   )
 }
