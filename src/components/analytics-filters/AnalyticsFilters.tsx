f6967cab5d9f6eb1c360d23324a8d347f18e8ce7
Amos <asidelnik@gmail.com>
2025-01-22 21:05:14 +0200

    115 - refactor: replace input fields with MUI TextField components in AnalyticsFilters for improved UI. Added onChange update location in global state.

--- src/components/analytics-filters/AnalyticsFilters.tsx
+++ src/components/analytics-filters/AnalyticsFilters.tsx
@@ -3,6 +3,7 @@ import { useAnalyticsFilters, useAnalyticsFiltersDispatch } from "../../contexts
 import CheckboxesTags from "../../shared/components/checkbox-tags/CheckboxTags";
 import { eventTypeNumOptions } from '../../constants/event-constants';
 import MonthYearPicker from '../../shared/components/date-pickers/MonthYearPicker';
+import { TextField } from '@mui/material';
 
 export default function AnalyticsFilters() {
   const filters = useAnalyticsFilters();
@@ -11,6 +12,9 @@ export default function AnalyticsFilters() {
   const updateFromDateHandler = (fromDate: Date | null) => filtersDispatch({ type: 'update-from-date', payload: fromDate });
   const updateToDateHandler = (toDate: Date | null) => filtersDispatch({ type: 'update-to-date', payload: toDate });
   const updateTypeHandler = (eventTypeId: string | null) => filtersDispatch({ type: 'update-event-type-id', payload: eventTypeId });
+  const updateLatitude = (latitude?: number) => filtersDispatch({ type: 'update-latitude', payload: latitude });
+  const updateLongitude = (longitude?: number) => filtersDispatch({ type: 'update-longitude', payload: longitude });
+  const updateRadius = (radius?: number) => filtersDispatch({ type: 'update-latitude', payload: radius });
 
   return (
     <>
@@ -35,18 +39,36 @@ export default function AnalyticsFilters() {
         />
 
         <div>
-          <label htmlFor="lat">Latitude: </label>
-          <input type="number" id="lat" />
+          <TextField
+            id="latitude"
+            label="Latitude"
+            type="number"
+            variant="outlined"
+            onChange={(e) => updateLatitude(Number(e.target.value))}
+            sx={{ width: "320px" }}
+          />
         </div>
 
         <div>
-          <label htmlFor="long">Longitude: </label>
-          <input type="number" id="long" />
+          <TextField
+            id="longitude"
+            label="Longitude"
+            type="number"
+            variant="outlined"
+            onChange={(e) => updateLongitude(Number(e.target.value))}
+            sx={{ width: "320px" }}
+          />
         </div>
 
         <div>
-          <label htmlFor="radius">Radius: </label>
-          <input type="number" id="radius" />
+          <TextField
+            id="longitude"
+            label="Radius"
+            type="number"
+            variant="outlined"
+            onChange={(e) => updateRadius(Number(e.target.value))}
+            sx={{ width: "320px" }}
+          />
         </div>
       </div>
     </>
