6640b14d5f84b9471185727d10421919a4b77378
Amos <asidelnik@gmail.com>
2025-01-26 14:44:15 +0200

    134 - feat: moved RecentEvents css & props interface to specific files

--- src/shared/components/charts/recent-events/RecentEvents.tsx
+++ src/shared/components/charts/recent-events/RecentEvents.tsx
@@ -1,17 +1,15 @@
+import c from './RecentEvents.module.scss';
 import { memo } from 'react';
-import { IRecentEvent } from '../../../types/IAnalytics';
+import { IRecentEvent } from '../../../../types/IAnalytics';
 import EventIcon from '@mui/icons-material/Event';
-import { dateToString, formatEventLocation } from '../../../utils/functions';
-
-interface IRecentEventsProps {
-  recentEvents: IRecentEvent[];
-}
+import { dateToString, formatEventLocation } from '../../../../utils/functions';
+import { IRecentEventsProps } from '../../../../types/IRecentEventsProps';
 
 export const RecentEvents = memo(({ recentEvents }: IRecentEventsProps) => {
   return (
     <>
       {recentEvents.map((event: IRecentEvent, index: number) => (
-        <div key={index} className="eventItem">
+        <div key={index} className={c.eventItem}>
           <EventIcon />
           <div>
             <h3>{event.title}</h3>
