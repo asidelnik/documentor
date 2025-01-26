b7c264312379964a3aa9a3fdacc62406ef068270
Amos <asidelnik@gmail.com>
2025-01-26 15:56:05 +0200

    134 - feat: Add map component to AnalyticsPage for selecting lat/lng and update filters

--- src/pages/analytics-page/AnalyticsPage.tsx
+++ src/pages/analytics-page/AnalyticsPage.tsx
@@ -5,15 +5,23 @@ import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import AnalyticsFilters from '../../components/analytics-filters/AnalyticsFilters';
 import { useFetchAnalytics } from '../../hooks/useFetchAnalytics';
 import { useState } from 'react';
-import { RecentEvents } from '../../shared/components/charts/RecentEvents';
+import { RecentEvents } from '../../shared/components/charts/recent-events/RecentEvents';
 import LineChart from '../../shared/components/charts/LineChart';
 import BarChart from '../../shared/components/charts/BarChart';
 import PieChart from '../../shared/components/charts/PieChart';
-
+import MapSelectLatLng from '../../shared/components/MapSelectLatLng';
+import { useAnalyticsFilters, useAnalyticsFiltersDispatch } from '../../contexts/analytics-filters-context';
+import { LatLngLiteral } from 'leaflet';
 
 export default function AnalyticsPage() {
   const [toggleAside, setToggleAside] = useState<boolean>(true);
   const { analyticsData } = useFetchAnalytics();
+  const [isShowMap, setIsShowMap] = useState<boolean>(false);
+  const filters = useAnalyticsFilters();
+  const filtersDispatch = useAnalyticsFiltersDispatch();
+  console.log('Analytics page', filters.lat, filters.long);
+
+  const updateLngLat = (center: LatLngLiteral) => filtersDispatch({ type: 'update-lng-lat', payload: center });
 
   return (
     <>
@@ -22,12 +30,11 @@ export default function AnalyticsPage() {
           <div className={c.asideToggle}>
             <IconButton onClick={() => setToggleAside(!toggleAside)} aria-label="toggle aside">
               {toggleAside ? <KeyboardArrowLeftIcon /> : <FilterAltOutlinedIcon />}
-              {/* Reset to default filtering */}
             </IconButton>
+            {/* Button to delete or reset all filters */}
           </div>
-
           <div className={toggleAside ? 'visible' : 'hidden'}>
-            <AnalyticsFilters />
+            <AnalyticsFilters isShowMap={isShowMap} setIsShowMap={setIsShowMap} />
           </div>
         </aside>
         <main>
@@ -47,6 +54,15 @@ export default function AnalyticsPage() {
               <RecentEvents recentEvents={analyticsData.recentEvents.data} />
             </div>
           </div>
+          {isShowMap &&
+            <div className={c.map}>
+              <MapSelectLatLng
+                lat={filters.lat}
+                lng={filters.long}
+                radius={filters.radius || 100}
+                setCenter={updateLngLat} />
+            </div>
+          }
         </main>
       </div>
     </>
