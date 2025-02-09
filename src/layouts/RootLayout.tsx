c1da56c88b5d8e680c294133924dc16d5430b163
Amos <asidelnik@gmail.com>
2025-02-09 20:14:15 +0200

    154 - refactor: Providers updated in RootLayout.

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,19 +1,19 @@
 import { Outlet } from "react-router-dom";
 import RootHeader from "../headers/RootHeader";
-import { FiltersProvider } from "../contexts/filters-context";
-import { EventsFiltersProvider } from "../contexts/events-filters-context";
-import { AnalyticsFiltersProvider } from "../contexts/analytics-filters-context";
+import { VideosFiltersProvider } from "../contexts/videos/VideosFiltersProvider";
+import { EventsFiltersProvider } from "../contexts/events/EventsFiltersProvider";
+import { AnalyticsFiltersProvider } from "../contexts/analytics/AnalyticsFiltersProvider";
 
 export default function RootLayout() {
   return (
     <>
       <EventsFiltersProvider>
-        <FiltersProvider>
+        <VideosFiltersProvider>
           <AnalyticsFiltersProvider>
             <RootHeader />
             <Outlet />
           </AnalyticsFiltersProvider>
-        </FiltersProvider>
+        </VideosFiltersProvider>
       </EventsFiltersProvider>
     </>
   );
